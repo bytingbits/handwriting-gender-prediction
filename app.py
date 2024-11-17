78ad5b9b4774dac4fd76e3e9c07ac84630b7f18f
Sharada <112688961+bytingbits@users.noreply.github.com>
2024-11-17 12:21:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ x_cold_train, x_cold_test, y_cold_train, y_cold_test = train_test_split(
 def hyperparameter_optimization(x_train, y_train):
     param_grid = {
         'C': [0.1, 1, 10, 100, 200, 400],  # Range of values for C
-        'gamma': ['scale', 'auto', 0.1, 1, 7, 10]  # Range of values for gamma
+        'gamma': ['scale', 'auto', 0.1, 1, 7, 10],  # Range of values for gamma
         'kernel': ['poly']
     }
 
